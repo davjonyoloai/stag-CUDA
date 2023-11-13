c7dcb3647e094834701a1194c6f7e7295be75677
Manfred.Stoiber <Manfred.Stoiber@gmail.com>
2023-11-13 20:17:52 +1300

    Made markers and falseCandidates public to be used by other applications

--- src/Stag.h
+++ src/Stag.h
@@ -10,6 +10,7 @@
 
 class Stag
 {
+
 	// if keepLogs is true, keep the intermediate results of the detection algorithm in the memory, to be dumped when asked (default: false)
 	bool keepLogs = false;
 	int errorCorrection;
@@ -24,15 +25,17 @@ class Stag
 	vector<cv::Mat> whiteLocs;
 
 	cv::Mat image;
-	vector<Marker> markers;
-	vector<Quad> falseCandidates;
 
 	// take readings from 48 code locations, 12 black border locations, and 12 white border locations
 	// thresholds and converts to binary code
 	Codeword readCode(const Quad &q);
 	void fillCodeLocations();
 	cv::Mat createMatFromPolarCoords(double radius, double radians, double circleRadius);
+
 public:
+    vector<Marker> markers;
+    vector<Quad> falseCandidates;
+
 	Stag(int libraryHD = 15, int errorCorrection = 7, bool inKeepLogs = false);
 	void detectMarkers(cv::Mat inImage);
 	void logResults(string path = "");
